4721f83f2aefd1a399e1e220506fbf29358e7802
Matt Gilbert <mattgilbert042@gmail.com>
2021-08-17 11:51:35 -0500

    resolving background overlay issues and project card spacing for mobile

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,7 +4,7 @@ const ProjectCard = ({ project: { image, title, description } }) => {
   return (
     <div className="col-xs-12 col-md-4">
       <div className="tile d-flex mx-auto" style={{ backgroundImage: `url(${image})` }}>
-        <div className="tile-content w-100 py-3 justify-content-center align-self-center text-center">
+        <div className="tile-content w-100 p-3 justify-content-center align-self-center text-center">
           <h5 className="tile-title">{title}</h5>
           <div className="tile-body">
             <p className="lead">{description}</p>
